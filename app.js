834ef1182f1a259579ac42420780847fb83cea19
Sofía Loisi <sofiaberenice.loisi@gmail.com>
2024-02-18 21:01:19 -0300

    agrego rutas y funcionalidad a cart

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ProductManager from './productManager.js';
 import CartManager from './cartManager.js';
 
+
 const app = express();
 const port = 8080;
 
@@ -100,7 +101,7 @@ app.delete("/api/products/:pid", async (req, res) => {
 
 app.post("/api/carts", async (req, res) => {
     try {
-        const newCart = await cartsManager.createCart(); 
+        const newCart = await cartsManager.createCart();
         if (newCart) {
             res.status(201).send({ success: "Nuevo carrito creado con exito", cart: newCart });
         } else {
@@ -108,6 +109,39 @@ app.post("/api/carts", async (req, res) => {
         }
     } catch (error) {
         console.log("Error al crear el carrito:", error);
+        res.status(500).send({ error: "Error interno del servidor" });
+    }
+});
+
+app.get("/api/carts/:cid", async (req, res) => {
+    try {
+        const cartId = req.params.cid;
+        const cart = await cartsManager.getCartById(cartId);
+        if (cart) {
+            res.status(200).send({ success: "Carrito encontrado", cart });
+        } else {
+            res.status(404).send({ error: "Carrito no encontrado" });
+        }
+    } catch (error) {
+        console.log("Error al obtener el carrito:", error);
+        res.status(500).send({ error: "Error interno del servidor" });
+    }
+});
+
+app.post("/api/carts/:cid/product/:pid", async (req, res) => {
+    try {
+        const cartId = req.params.cid;
+        const productId = req.params.pid;
+        const quantity = req.body.quantity || 1; //cantidad 1 por defecto si no se especifica.
+
+        const updatedCart = await cartsManager.addProductToCart(cartId, productId, quantity);
+        if (updatedCart) {
+            res.status(200).send({ seccess: "Producto agregado al carrito correctamente", cart: updatedCart });
+        } else {
+            res.status(404).send({ error: "No se pudo agregar el producto al carrito: el carrito o el producto no existe." });
+        }
+    } catch (error) {
+        console.log("Error al agregar el producto al carrito:", error);
         res.status(500).send({ error: "Error interno del servidor" });
     }
 });
--- cartManager.js
+++ cartManager.js
@@ -1,6 +1,8 @@
 import { randomUUID } from "node:crypto";
 import fs from 'fs';
+import ProductManager from './productManager.js';
 
+const productsManager = new ProductManager('./data/products.json');
 
 
 class CartManager {
@@ -22,6 +24,55 @@ class CartManager {
             return cart;
         } catch (error) {
             console.log('Error creando el carrito:', error);
+            return null;
+        }
+    }
+
+    getCartById = async (cartId) => {
+        try {
+            let carts = JSON.parse(fs.readFileSync(this.cartPath, 'utf-8'));
+            const cart = carts.find(cart => cart.id === cartId);
+            if (cart) {
+                return cart;
+            } else {
+                console.log('Carrito no encontrado');
+                return null;
+            }
+        } catch (error) {
+            console.log('Error obteniendo el carrito:', error);
+            return null;
+        }
+    }
+
+    addProductToCart = async (cartId, productId, quantity) => {
+        try {
+            let carts = JSON.parse(fs.readFileSync(this.cartPath, 'utf-8'));
+            const cart = carts.find(cart => cart.id === cartId);
+    
+            if (cart) {
+                const productInCart = cart.products.find(product => product.productId === productId);
+    
+                if (productInCart) {
+                    productInCart.quantity += quantity;
+                } else {
+                    const product = await productsManager.getProductById(productId);
+                    if (product){
+                        cart.products.push({ productId, quantity });
+                    }else{
+                        console.log ('El producto no existe');
+                        return null;
+                    }
+                }
+    
+                fs.writeFileSync(this.cartPath, JSON.stringify(carts, null, 2));
+                console.log('Producto agregado al carrito exitosamente');
+                return cart;
+            } else {
+                console.log('Carrito no encontrado');
+                return null;
+            }
+        } catch (error) {
+            console.log('Error agregando producto al carrito:', error);
             return null;
         }
     }
