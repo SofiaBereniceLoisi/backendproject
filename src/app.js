0a49c604b7e01471cc36c772523a06259116d56f
Sofía Loisi <sofiaberenice.loisi@gmail.com>
2024-03-05 02:27:16 -0300

    agregando socket.io + config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import Handlebars from 'express-handlebars';
 import __dirname from './utils.js';
+import { Server } from 'socket.io';
 
 // IMPORT ROUTES
 import productsRouter from './routes/productsRouter.js';
 import cartRouter from './routes/cartRouter.js';
-import mainRouter from './routes/mainRouter.js';
+// import mainRouter from './routes/mainRouter.js';
 import viewsRouter from './routes/viewsRouter.js';
 
 const app = express();
@@ -14,6 +15,7 @@ const port = 8080;
 // MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`))
 
 // Carpeta vistas y motor de plantillas
 app.set('views', `${__dirname}/views`);
@@ -21,11 +23,24 @@ app.set('view engine', 'handlebars');
 app.engine('handlebars', Handlebars.engine())
 
 // ROUTES
-app.use(mainRouter);
+// app.use(mainRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/products', productsRouter);
 app.use(viewsRouter);
 
+// HTTP Server
+const server = app.listen(port, () => { console.log(`Servidor escuchando en el puerto ${port}`); });
 
+// Websocket Server
+const io = new Server(server); //socket.io
 
-app.listen(port, () => { console.log(`Servidor escuchando en el puerto ${port}`); });
+io.on('connection', (socket) => {
+
+    console.log('Client connected!');
+
+    socket.on('message', (data) => {
+        console.log(data)
+        // io.emit('log', data);
+
+    })
+})
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,4 +1,20 @@
 import { Router } from "express";
 const viewsRouter = Router();
 
+import ProductManager from '../productManager.js'; 
+const productsManager = new ProductManager('./data/products.json'); 
+
+viewsRouter.get('/', async (req, res) => {
+    try {
+        // Obtener la lista de productos
+        const products = await productsManager.getProducts();
+        // Renderizar la vista 'home' y pasar la lista de productos
+        res.render('home', { products: products });
+    } catch (error) {
+        console.log('Error al obtener productos:', error);
+        res.status(500).send({ error: 'Error al obtener productos' });
+    }
+});
+
+
 export default viewsRouter;
